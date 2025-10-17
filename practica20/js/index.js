5fb4ed326d59c5472311c4e73f2e36e38e59392c
SebastianMCCC <s.miramontes456@gmail.com>
2025-10-17 07:47:01 -0600

    Add files via upload

---
+++ practica20/js/index.js
@@ -0,0 +1,18 @@
+let nombre= "Miguel Sebastian";
+let apellido= "Miramontes Castro";
+let nombrecompleto= nombre+' '+apellido;
+let numero= 250;
+let numero2= 300;
+
+var numero3=100;
+numero4=50;
+
+console.log(nombrecompleto,numero3,numero4);
+let suma= numero + numero2 + numero3 + numero4
+console.log(suma)
+let resta= numero3 - numero2;
+console.log('El resultado es: ',resta)
+let multiplicacion= numero * numero2;
+console.log('El resultado es: ',multiplicacion)
+let division= numero3 / numero4;
+console.log('El resultado es: ',division)
